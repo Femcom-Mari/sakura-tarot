ca7c10139f35dfe9a7e4b3fd3ea8b5b9973b9d90
einartech <imalovelyghost13@gmail.com>
2025-02-07 13:07:15 +0100

    component/home added and fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
 import styles from "./Home.module.css";
+import Header from "../../components/header/Header";
+
 export default function Home() {
   return (
     <>
       <h2 className={styles.titulo1}>COMPONENTE PAGINA HOME</h2>
+      <Header />
     </>
   );
 }
