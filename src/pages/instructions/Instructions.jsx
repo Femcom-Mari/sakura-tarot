748e9e96a571f4853672db605eae03c840179df7
marianamarinflor622 <marianamarinflor@gmail.com>
2025-02-10 14:43:32 +0100

    linked Footer  on pages

--- src/pages/instructions/Instructions.jsx
+++ src/pages/instructions/Instructions.jsx
@@ -1,8 +1,11 @@
 import styles from "./Instructions.module.css";
+
+import Footer from "../../components/footer/Footer";
 export default function Instructions() {
   return (
     <>
       <h2 className={styles.titulo1}>COMPONENTE PAGINA INSTRUCTIONS</h2>
+      <Footer/>
     </>
   );
 }
--- src/pages/reading/Reading.jsx
+++ src/pages/reading/Reading.jsx
@@ -1,8 +1,10 @@
 import styles from "./Reading.module.css";
+import Footer from "../../components/footer/Footer";
 export default function Reading() {
   return (
     <>
       <h2 className={styles.titulo1}>COMPONENTE PAGINA READING</h2>
+      <Footer/>
     </>
   );
 }
