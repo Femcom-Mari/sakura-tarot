df340f04acb24ea586aa0322f252442d44d16fe0
marianamarinflor622 <marianamarinflor@gmail.com>
2025-02-10 11:52:13 +0100

    first version header

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import Header from "../../components/header/Header";
 export default function Home() {
   return (
     <>
-      <h2 className={styles.titulo1}>COMPONENTE PAGINA HOME</h2>
+     
       <Header />
     </>
   );
