c036a64d089711c55583773cd915a31d8156e8b2
Israel Espin <israel.espin92@gmail.com>
2025-02-10 15:08:40 +0100

    creation and loading of buttons

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import styles from "./Home.module.css";
 import Header from "../../components/header/Header";
+import Button from "../../components/button/Button"
 
 export default function Home() {
   return (
     <>
       <h2 className={styles.titulo1}>COMPONENTE PAGINA HOME</h2>
       <Header />
+      <Button
+      text="Registrarse"/>
     </>
   );
 }
--- src/pages/instructions/Instructions.jsx
+++ src/pages/instructions/Instructions.jsx
@@ -1,8 +1,14 @@
 import styles from "./Instructions.module.css";
+import Button from "../../components/button/Button"
+
+
+
 export default function Instructions() {
   return (
     <>
       <h2 className={styles.titulo1}>COMPONENTE PAGINA INSTRUCTIONS</h2>
-    </>
+      <Button
+       text="Comenzar"/>
+        </>
   );
 }
