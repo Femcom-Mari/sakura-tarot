0fd4bcb7ca2cbf2a13127b5b9df4307cccde9bce
kat-lev <kat@katleverton.com>
2025-02-12 19:20:58 +0100

    adapt router and pages

--- src/config/router/Router.jsx
+++ src/config/router/Router.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import Home from "../../pages/home/Home";
 import History from "../../pages/history/History";
 import Instructions from "../../pages/instructions/Instructions";
-import Introduction from "../../pages/introduction/Introduction";
 import Reading from "../../pages/reading/Reading";
 
 import "../../../../sakura-tarot/src/index.css";
@@ -12,10 +11,10 @@ export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/history" element={<History />} />
         <Route path="/instructions" element={<Instructions />} />
-        <Route path="/introduction" element={<Introduction />} />
         <Route path="/reading" element={<Reading />} />
       </Routes>
     </BrowserRouter>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,16 @@
 import styles from "./Home.module.css";
-import Navbar from "../../components/Navbar/Navbar";
-import Button from "../../components/button/Button";
 import Footer from "../../components/footer/Footer";
-export default function Home() {
+import Navbar from "../../components/Navbar/Navbar";
+import Form from "./../../components/form/Form.jsx";
+
+function Home() {
   return (
     <>
-     
-      <Navbar/>
-      <Button
-      text="Registrarse"/>
-
-      <Footer/>    
+    <Navbar/>
+    <Form />
+    <Footer/>
     </>
   );
 }
+
+export default Home
