909172a6d44049d37e41a4c3d6831777799378cc
marianamarinflor622 <marianamarinflor@gmail.com>
2025-02-13 12:29:26 +0100

    audio integrated into the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,46 @@
-import { NavLink } from "react-router";
+import { useState, useRef, useEffect } from "react";
+import { NavLink } from "react-router-dom"; 
 import styles from "../Navbar/Navbar.module.css";
-import refresh from '../../assets/images/refresh-icon.png';
-import sound from '../../assets/images/sound-on-icon.png';
+import refresh from "../../assets/images/refresh-icon.png";
+import soundOn from "../../assets/images/sound-on-icon.png"; 
+import soundOff from "../../assets/images/sound-off-icon.png"; 
+import audioFile from "../../audio/background-music.mp3"; 
 
 export default function Home() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    audioRef.current = new Audio(audioFile);
+    audioRef.current.loop = true; 
+  }, []);
+
+  const toggleSound = () => {
+    if (!audioRef.current) return; 
+
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
   return (
-    
-         <div>
-             <header>
-             <div className={styles.container}>
-             <NavLink className={styles.sol} to="/home">
-             <h1>Sol Tarot</h1>
-             </NavLink>
-             <button className  ={styles.refresh}>
-               <img src={refresh} alt="refresh" />
-              </button>
-              <button className  ={styles.sound}>
-               <img src={sound} alt="refresh" />
-              </button>
-            </div>
-                 </header>
-             </div>
-        
-    );
+    <div>
+      <header>
+        <div className={styles.container}>
+          <NavLink className={styles.sol} to="/home">
+            <h1>Sol Tarot</h1>
+          </NavLink>
+          <button className={styles.refresh}>
+            <img src={refresh} alt="refresh" />
+          </button>
+          <button className={styles.sound} onClick={toggleSound}>
+            <img src={isPlaying ? soundOn : soundOff} alt="sound toggle" />
+          </button>
+        </div>
+      </header>
+    </div>
+  );
 }
